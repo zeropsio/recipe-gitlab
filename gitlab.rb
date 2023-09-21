1adc02f0b400acd89d875ca3acc3790b50af3c5b
Matěj Pavlíček <tikinang@gmail.com>
2023-09-21 20:56:43 +0200

    init commit

---
+++ gitlab.rb
@@ -0,0 +1,8 @@
+postgresql['enable'] = false
+
+gitlab_rails['db_adapter'] = 'postgresql'
+gitlab_rails['db_encoding'] = 'utf8'
+gitlab_rails['db_host'] = 'db'
+gitlab_rails['db_port'] = 5432
+gitlab_rails['db_username'] = '$$db_user$$'
+gitlab_rails['db_password'] = '$$db_password$$'
